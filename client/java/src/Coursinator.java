d8c4482a51a9a285ac34420a2d501d1711054c16
Matthew Maynes <matthewmaynes1@gmail.com>
2014-10-06 13:11:28 -0400

    [Java] Added entry point to program

---
+++ client/java/src/Coursinator.java
@@ -0,0 +1,21 @@
+/**
+ * Coursinator
+ *
+ * Coursinator allows users to input their completed time schedule and get suggestions
+ * for their future courses.
+ *
+ * @version 0.0.0
+ * @date October 6, 2014
+ * @author Matthew Maynes, Cameron Blanchard and Kevin Cox
+ */
+ 
+ 
+public class Coursinator{
+
+	public static void main(String[] args){
+		
+		
+		
+	}
+	
+}
