7ccfc609bc9cdc95684ad313ca3effb0b1757e4b
Kevin Cox <kevincox@kevincox.ca>
2014-10-09 19:42:24 -0400

    Update timetable endpoint to XML.

--- server/api/timetables.php
+++ server/api/timetables.php
@@ -1,17 +1,19 @@
 <?php
 	if ($_SERVER['REQUEST_METHOD'] != 'POST'){
-		http_response_code(405);
-		echo '{"e":1,"msg":"Method must be POST."}';
 		exit;
 	}
 	
 	require_once('lib/json.php');
 	
-	$in = readjson();
-	
-	$r = ['e' => 0,
-		'timetables' => [],
-	];
-	
-	header("Content-Type", "application/javascript; charset=utf-8");
-	echo json_encode($r);
+	switch ($_SERVER['REQUEST_METHOD']) {
+	case "POST":
+		header('Content-Type: text/xml; charset=utf-8');
+		$r = '<response e="0"><timetables>';
+		$r .= '</timetables></response>';
+		return $r;
+		break;
+	default:
+		http_response_code(405);
+		header('Content-Type: text/xml; charset=utf-8');
+		echo '<response e="1" msg="Method must be POST."/>';
+	}
